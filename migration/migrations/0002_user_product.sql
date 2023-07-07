093e079a822ef641aaf9ba3e3a24c15899d8be1b
AnisaForWork <138590484+AnisaForWork@users.noreply.github.com>
2023-07-07 14:59:23 +0300

    Update 0002_user_product.sql

--- migration/migrations/0002_user_product.sql
+++ migration/migrations/0002_user_product.sql
@@ -13,17 +13,9 @@ CREATE TABLE  IF NOT EXISTS products(
     FOREIGN KEY (userId)  REFERENCES users (id) 
 );
 
-CREATE TABLE IF NOT EXISTS prchecks( 
-    filename varchar(45) NOT NULL,
-    barcode  varchar(10) NOT NULL,
-    CONSTRAINT u_pkey PRIMARY KEY (filename),
-    CONSTRAINT products_files_fk 
-    FOREIGN KEY (barcode)  REFERENCES products (barcode) 
-);
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
 DROP TABLE  IF EXISTS products;
-DROP TABLE  IF EXISTS prchecks;
 -- +goose StatementEnd
